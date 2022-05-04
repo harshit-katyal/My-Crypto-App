022a895ce4f5d48a0a2101bcd93428ffd74e174d
harshit-katyal <hkatyal345@gmail.com>
2022-05-04 16:54:05 +0530

    created Genesis Block

---
+++ block.js
@@ -0,0 +1,20 @@
+const{ GENESIS_DATA } = require('./config')
+class Block
+{
+    constructor({timestamp , lastHash, hash, data})
+     {
+            this.timestamp = timestamp
+            this.lastHash = lastHash
+             this.hash = hash
+             this.data = data
+     }
+
+     static genesis() //a static function that is creating a new Block instance without directly using the constructor. They are called factory methods.
+     {
+         return new this(GENESIS_DATA);
+     }
+
+     
+}
+ 
+module.exports = Block;
