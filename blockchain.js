87b770925035d3291014e4c71c80f0a050583eb4
harshit-katyal <hkatyal345@gmail.com>
2022-05-09 10:28:11 +0530

    added diffculty jump check>1

--- blockchain.js
+++ blockchain.js
@@ -26,15 +26,19 @@ class Blockchain
         {
             const {timestamp , lastHash, hash, nonce,difficulty,data} = chain[i];
             const actualLastHash = chain[i-1].hash;
-            
+            const lastDifficulty = chain[i-1].difficulty
 
             if(lastHash !== actualLastHash)return false;
             
             const validatedHash  = cryptoHash(timestamp, lastHash,data,nonce,difficulty);
 
             if(hash!=validatedHash)return false;
+            
+            if(Math.abs(lastDifficulty-difficulty)>1)return false;
+
         }
 
+         
         return true;
     }
     replaceChain(chain)  //this is not a static method because replacing the chain is based on an individual instance 
